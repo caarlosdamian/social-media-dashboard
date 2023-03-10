78d6bcc76b1a798a36d2bbb351accd096f38bc9e
caarlosdamian <dapecarlos@gmail.com>
2023-03-09 23:31:42 -0600

    feat(context): added context provider

---
+++ src/context/Theme.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, Dispatch, FC, SetStateAction } from "react";
+
+export type ThemeContextType = {
+  dark: boolean;
+  setDark: Dispatch<SetStateAction<boolean>>;
+};
+
+type Props = {
+  children: string | JSX.Element | JSX.Element[];
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export const ThemeProvider = ({ children }: Props) => {
+  const [dark, setDark] = React.useState<boolean>(false);
+
+  return (
+    <ThemeContext.Provider value={{ dark: dark, setDark }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
